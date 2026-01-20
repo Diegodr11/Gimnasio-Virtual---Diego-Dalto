b3729964efe12eb8a6835f460be17521bf4b899f
Diegodr11 <141876601+Diegodr11@users.noreply.github.com>
2026-01-19 21:55:59 -0300

    Update Gimnasio Virtual - Dalto.sql

--- Gimnasio Virtual - Dalto.sql
+++ Gimnasio Virtual - Dalto.sql
@@ -1,14 +1,12 @@
-
 -- Creación de la base de datos
 CREATE DATABASE gimnasio_virtual;
 
--- Utilizar la base de datos creada
 USE gimnasio_virtual;
 
 -- Tabla: Usuarios
 CREATE TABLE Usuarios (
     UserID INT AUTO_INCREMENT PRIMARY KEY,
-    Nombre VARCHAR(50),
+    Nombre VARCHAR(50) NOT NULL,
     Edad INT,
     Altura DECIMAL(5,2),
     Peso DECIMAL(5,2),
@@ -18,7 +16,7 @@ CREATE TABLE Usuarios (
 -- Tabla: Rutinas de Ejercicios
 CREATE TABLE Rutinas_de_Ejercicios (
     RutinaID INT AUTO_INCREMENT PRIMARY KEY,
-    Nombre VARCHAR(100),
+    Nombre VARCHAR(100) NOT NULL,
     Descripcion TEXT,
     Nivel VARCHAR(20)
 );
@@ -26,7 +24,7 @@ CREATE TABLE Rutinas_de_Ejercicios (
 -- Tabla: Videos de Ejercicios
 CREATE TABLE Videos_de_Ejercicios (
     VideoID INT AUTO_INCREMENT PRIMARY KEY,
-    Nombre VARCHAR(100),
+    Nombre VARCHAR(100) NOT NULL,
     Descripcion TEXT,
     URL VARCHAR(200)
 );
@@ -34,24 +32,24 @@ CREATE TABLE Videos_de_Ejercicios (
 -- Tabla: Planes Nutricionales
 CREATE TABLE Planes_Nutricionales (
     PlanID INT AUTO_INCREMENT PRIMARY KEY,
-    Nombre VARCHAR(100),
-    Descripcion TEXT
+    Nombre VARCHAR(100) NOT NULL,
+    Descripcion VARCHAR(100)
 );
 
 -- Tabla: Suscripciones o Compras
 CREATE TABLE Suscripciones_o_Compras (
     SuscripcionID INT AUTO_INCREMENT PRIMARY KEY,
-    Fecha_de_Compra DATE,
-    Tipo_de_Suscripcion VARCHAR(50),
-    UserID INT,
+    Fecha_de_Compra DATE NOT NULL,
+    Tipo_de_Suscripcion VARCHAR(50) NOT NULL,
+    UserID INT NOT NULL,
     FOREIGN KEY (UserID) REFERENCES Usuarios(UserID)
 );
 
 -- Tabla: Usuarios_Rutinas (Tabla Intermedia)
 CREATE TABLE Usuarios_Rutinas (
     Usuario_RutinaID INT AUTO_INCREMENT PRIMARY KEY,
-    UserID INT,
-    RutinaID INT,
+    UserID INT NOT NULL,
+    RutinaID INT NOT NULL,
     Fecha_de_Inicio DATE,
     Fecha_de_Fin DATE,
     FOREIGN KEY (UserID) REFERENCES Usuarios(UserID),
@@ -61,8 +59,8 @@ CREATE TABLE Usuarios_Rutinas (
 -- Tabla: Rutinas_Videos (Tabla Intermedia)
 CREATE TABLE Rutinas_Videos (
     Rutina_VideoID INT AUTO_INCREMENT PRIMARY KEY,
-    RutinaID INT,
-    VideoID INT,
+    RutinaID INT NOT NULL,
+    VideoID INT NOT NULL,
     Orden INT,
     FOREIGN KEY (RutinaID) REFERENCES Rutinas_de_Ejercicios(RutinaID),
     FOREIGN KEY (VideoID) REFERENCES Videos_de_Ejercicios(VideoID)
@@ -71,7 +69,7 @@ CREATE TABLE Rutinas_Videos (
 -- Tabla: Usuarios_Planes (Relación Uno a Uno)
 CREATE TABLE Usuarios_Planes (
     UserID INT PRIMARY KEY,
-    PlanID INT,
+    PlanID INT NOT NULL UNIQUE,
     Fecha_de_Inicio DATE,
     Fecha_de_Fin DATE,
     FOREIGN KEY (UserID) REFERENCES Usuarios(UserID),
